03fd7c811766e67a2679f86a52281348ae96ae60
Komalpreet Kaur <komalpreetkaur@Komalpreets-Laptop.local>
2025-03-09 20:54:33 -0600

    Finished US 02.04.01 +drop down menu for choosing social situations

--- code/app/src/main/java/com/example/myapplication/NewMoodActivity.java
+++ code/app/src/main/java/com/example/myapplication/NewMoodActivity.java
@@ -18,26 +18,18 @@ public class NewMoodActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.newmood);
-
-
         Spinner moodSpinner = findViewById(R.id.selectMoodText);
-
         String[] moods = {"Happy", "Sad", "Excited", "Angry", "Relaxed", "Confused"};
-
-
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<String> adapter = new ArrayAdapter<>(
                 this,
                 android.R.layout.simple_spinner_item, // Default layout for the closed spinner
                 moods
         );
-
         // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
         // Apply the adapter to the spinner
         moodSpinner.setAdapter(adapter);
-
         // Set an item selection listener
         moodSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -50,9 +42,36 @@ public class NewMoodActivity extends AppCompatActivity {
             public void onNothingSelected(AdapterView<?> adapterView) {
                 // Do nothing
             }
-        });
 
-        // Set up the top app bar in new_mood.xml
+        });
+        Spinner situationSpinner = findViewById(R.id.selectSituationText);
+        String []social_situation = {"Alone","One other person","Several people","A crowd"};
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<String> situationadapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_spinner_item, // Default layout for the closed spinner
+                social_situation
+        );
+
+        // Specify the layout to use when the list of choices appears
+        situationadapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Apply the adapter to the spinner
+        situationSpinner.setAdapter(situationadapter);
+
+        // Set an item selection listener
+        situationSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        @Override
+        public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+        String selectSituationText = adapterView.getItemAtPosition(position).toString();
+       Toast.makeText(getApplicationContext(), "Selected: " + selectSituationText, Toast.LENGTH_SHORT).show();
+                                                       }
+       @Override
+       public void onNothingSelected(AdapterView<?> adapterView) {
+        // Do nothing
+        }});
+
+            // Set up the top app bar in new_mood.xml
         topAppBar = findViewById(R.id.topAppBar);
         setSupportActionBar(topAppBar);
 
